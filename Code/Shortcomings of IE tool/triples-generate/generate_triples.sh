c5a5a8d7b44ae2d1bb0d3d1489d019f28803b5bf
sahuarchana7 <archana71989@yahoo.com>
2023-06-12 22:45:22 +0530

    Add files via upload

--- Code/Shortcomings of IE tool/triples-generate/generate_triples.sh
+++ Code/Shortcomings of IE tool/triples-generate/generate_triples.sh
@@ -1,1 +1,9 @@
-
+./arkref.sh -input m.txt
+python pro_reso33.py
+python 2copy.py
+java -jar clausie.jar -vlf /location-where-to-store-output-files/1_inp1.txt -o /location-where-to-store-output-files/outm.txt
+./arkref.sh -input s1.txt
+python pro_reso3.py
+python 3copy1_2.py
+java -jar clausie.jar -vlf /location-where-to-store-output-files/2_inp1.txt -o /location-where-to-store-output-files/outs.txt
+python triples_extract_ie.py
